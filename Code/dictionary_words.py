dd82c330250a23af4f50c0829fb8beedba143f30
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-04 10:35:36 -0700

    optimized two files for speed and more modular code

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -3,19 +3,23 @@ import sys
 
 number = input('Enter number of words: ') #takes user input
 
-def random_sentence():
-    words_list = [] #creates list object
-    with open('/usr/share/dict/words') as file: #accessing word file
-        text = file.read().strip().split() #reads file, strips leading/trailing chars, and retuns a list of strings
-        for word in text:
-            words_list.append(word) #appending words from file to a list
-            word_array = random.choices(words_list, k= int(number)) #picks a random word from list and stores in an array
-            sentence = ' '.join(word_array) + '.' #joins words into a sentence
-            sentence = sentence.capitalize() #capitalizes beginning of sentence
+def get_words():
+  words_list = []  # creates list object
+  with open('/usr/share/dict/words') as file:  # accessing word file
+    text = file.read().strip().split() #reads file, strips leading/trailing chars, and retuns a list of strings
+    for word in text:
+      words_list.append(word)
+    return words_list
 
-        return sentence
+
+def random_sentence(words):
+  word_array = random.choices(words, k= int(number)) #picks a random word from list and stores in an array
+  sentence = ' '.join(word_array) + '.' #joins words into a sentence
+  sentence = sentence.capitalize() #capitalizes beginning of sentence
+  return sentence
 
     
 if __name__ == '__main__':
-  test = random_sentence()
+  words_list = get_words()
+  test = random_sentence(words_list)
   print(test)
--- Code/histogram.py
+++ Code/histogram.py
@@ -1,9 +1,13 @@
 import random
 
-def histogram(file):
-    with open(file) as f: #access file
-        dict = {} #creates empty dict
-        text = f.read().strip().split() #reads file, strips leading/trailing chars, and retuns a list of strings
+def histo_file (file):
+    with open(file) as f:  # access file
+        # reads file, strips leading/trailing chars, and retuns a list of strings
+        text = f.read().strip().split()
+        return text
+
+def histogram(text):
+    dict = {}  # creates empty dict
     for word in text: #adding word and word frequency as key,val pairs
         if word in dict:
             dict[word] += 1
@@ -23,7 +27,9 @@ def frequency(word, histogram):
         return "That word is not in the histogram"  #returns error msg
 
 if __name__ == '__main__':
-    histo = histogram('siddhartha.txt')
+    histo_text = histo_file('siddhartha.txt')
+    histo = histogram(histo_text)
+    print(histo)
 
     print(unique_words(histo))
 
