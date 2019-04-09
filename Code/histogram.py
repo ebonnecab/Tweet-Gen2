545a6bd91168a83a0940525ff797c4569dae95cc
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-09 16:37:53 -0700

    added function to tokenize and clean words for a better histogram

--- Code/histogram.py
+++ Code/histogram.py
@@ -4,23 +4,41 @@ def get_words(file):
     words_list = []
     with open(file) as f:  # access file
         #access each line of file
-        for line in f:
-            #splits line into list of words
-            text = line.split()
-            for word in text: #accessing each word
-                word.strip() #strips trailing/leading chars
-                words_list.append(word) #appending words to list
-    return words_list
+        text = f.read()
+        # for line in f:
+        #     #splits line into list of words
+        #     text = line.split()
+        #     for word in text: #accessing each word
+        #         word.strip() #strips trailing/leading chars
+        #         words_list.append(word) #appending words to list
+    return text
 
+#split text into word tokens
+from nltk import word_tokenize
+import string
+from nltk.corpus import stopwords
+
+def get_tokens(text):
+    #split text into word tokens
+    tokens = word_tokenize(text)
+    #converts tokens to lowercase
+    tokens = [word.lower() for word in tokens]
+    #removes punctuation from each word
+    table = str.maketrans('', '', string.punctuation) 
+    stripped = [w.translate(table) for w in tokens]
+    #remove non-alphabetic tokens
+    words = [word for word in stripped if word.isalpha()]
+
+    return words
 
 ''' 
 Dictionary Implementation of Histogram
 adding word and word frequency as key,val pairs
 '''
         
-def histogram(text):
+def histogram(words):
     dict = {}  # creates empty dict
-    for word in text:
+    for word in words:
         #check if word is in dict
         if word in dict:
         #increase count by 1
@@ -136,23 +154,25 @@ def tuplegram(text):
     return words_list
 
 if __name__ == '__main__':
-    histo_text = get_words('animals.txt')
-    histo = histogram(histo_text)
+    histo_text = get_words('siddhartha.txt')
+    clean_text = get_tokens(histo_text)
+    histo = histogram(clean_text)
+    # print(histo)
     # sorted_histo = sorter(histo)
     # print(sorted_histo)
     
-    # print(word_count_dict(histo))
+    print(word_count_dict(histo))
     # print_table(histo)
     # print(histo)
     # print(unique_words(histo))
     # print(frequency('he', histo))
 
-    listo = listogram(histo_text)
-    print(listo)
+    # listo = listogram(histo_text)
+    # print(listo)
     # print(unique_words(listo))
     # print(frequency('fish', listo))
 
-    # tuplegram = tuplegram()
+    # tuplegram = tuplegram(histo_text)
     # print(tuplegram)
 
     # histo_file('histo.txt', histo)
