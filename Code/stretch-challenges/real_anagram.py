0427f06f5a9f2ffa04de649a72364b76cdacf3b5
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-09 13:17:47 -0700

    refactored code to accept diff files

--- Code/stretch-challenges/real_anagram.py
+++ Code/stretch-challenges/real_anagram.py
@@ -1,20 +1,23 @@
 from collections import defaultdict
 
-def get_words():
+def get_words(file):
   words_list = []  # creates list object
-  with open('/usr/share/dict/words') as file:  # accessing word file
+  with open(file) as f:  # accessing word file
     # reads file, strips leading/trailing chars, and retuns a list of strings
-    text = file.read().strip().split()
+    text = f.read().strip().split()
     for word in text:
       words_list.append(word)
     return words_list
 
 def get_anagrams(words_list):
+    #default dict creates new entries instead of throwing key error if key doesn't exist
     dict = defaultdict(list)
     for word in words_list:
         key = "".join(sorted(word))
         dict[key].append(word)
     return dict
 
-def print_anagrams():
-    return
+if __name__ == '__main__':
+    words = get_words('/usr/share/dict/words')
+    anagrams = get_anagrams(words)
+    print(anagrams)
