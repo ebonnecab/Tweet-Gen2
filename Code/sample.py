3c151fda96437fda5321848a2481951987e1fd98
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-11 12:01:09 -0700

    attempted sampling with list method. not quite working

--- Code/sample.py
+++ Code/sample.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 import sys
+
+#importing functions from histogram file
 from histogram import histogram
 from histogram import get_words
 from histogram import get_tokens
@@ -51,15 +53,44 @@ def results_histogram(outcomes):
     
     return results
 
+'''
+List Implementation of sampling function
+'''
+def print_probability2(listogram):
+    #gets total word count
+    word_count = len(listogram)
+
+    #printing out probability of word being picked
+    for index in listogram:
+        print("{} = {}".format(index[0],index[1]/word_count))
+
+def list_sampling(listogram):
+    #gets total word count
+    word_count = sum(listogram)
+    
+    #creating chance variable
+    chance = 0
+    random_num = random.random()
+
+    #returns random sample words
+    for index in listogram:
+        chance += index[1]/word_count 
+        if chance >= random_num:
+            return index[0]
+
 if __name__ == '__main__':
     histo_text = get_words('animals.txt')
     clean_text = get_tokens(histo_text)
     histo = histogram(clean_text)
     sample_word = sample(histo)
-    probability = print_probability(histo)
+    # probability = print_probability(histo)
     # print(sample_word)
     outcomes = test_probability(histo)
     results = results_histogram(outcomes)
-    print(results)
-    
+    # print(results)
+    # listo = listogram(clean_text)
+    # sample_word2 = list_sampling(listo)
+    # print(sample_word2)
+    # probability2 = print_probability2(listo)
+
    
