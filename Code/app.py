e2bf807bbb7f880f0e51c5400f700e35d4ac89c6
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-11 17:24:22 -0700

    working basic flask app shows random word

--- Code/app.py
+++ Code/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from histogram import histogram
+from histogram import histogram
+from histogram import get_words
+from histogram import get_tokens
+from histogram import listogram
+from sample import sample
+
+HTML="""<html><head><title>My App</title></head>
+        <body><h2>{}</h2></body"""
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Ebonne!'
+    histo_text = get_words('animals.txt')
+    clean_text = get_tokens(histo_text)
+    histo = histogram(clean_text)
+    random_word = sample(histo)
+    return HTML.format(random_word)
