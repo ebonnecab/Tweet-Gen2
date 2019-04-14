21d560e87c8289613d0ebbc5043d74fad1d8f09c
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-14 11:23:12 -0700

    changed procfile

--- Code/app.py
+++ Code/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from histogram import histogram
 from histogram import histogram
 from histogram import get_words
+from histogram import get_tokens
 from histogram import listogram
 from sample import sample
 from rearrange import sentence_maker
@@ -13,7 +14,8 @@ app = Flask(__name__)
 @app.route('/words/<int:num>')
 def hello_world(num):
     histo_text = get_words('siddhartha.txt')
-    histo = histogram(histo_text)
+    clean_text = get_tokens(histo_text)
+    histo = histogram(clean_text)
     random_word = sample(histo)
     random_words = []
     for i in range(num):
@@ -21,7 +23,5 @@ def hello_world(num):
     random_sentence = sentence_maker(random_words)
     return HTML.format(random_sentence)
 
- if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
