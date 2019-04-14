8ca7c7238f9b9a30c38bc502fe94026ef7e09219
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-14 12:05:00 -0700

    commented out but kept old code

--- Code/app.py
+++ Code/app.py
@@ -27,5 +27,19 @@ def tweet_gen():
     random_sentence = sentence_maker(random_words)
     return HTML.format(random_sentence)
 
+#implementation where user can generate as many words as they want
+# @app.route('/words/<int:num>')
+# def tweet_gen(num):
+#     histo_text = get_words('siddhartha.txt')
+#     clean_text = get_tokens(histo_text)
+#     histo = histogram(clean_text)
+#     random_word = sample(histo)
+#     random_words = []
+#     for i in range(num):
+#         random_words.append(sample(histo))
+#     random_sentence = sentence_maker(random_words)
+#     return HTML.format(random_sentence)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
