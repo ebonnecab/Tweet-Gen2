1d0e2f94146fa11b5459216132a77fb5f4408b7b
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-16 13:18:19 -0700

    listogram passing all tests

--- Code/listogram.py
+++ Code/listogram.py
@@ -19,20 +19,39 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        for index in range(len(self)):
+            if self[index][0] == word:
+                self[index][1] += count
+                self.tokens += count
+                break
+        else:
+            self.types += 1 
+            self.tokens += count
+            self.append([word, count])
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        for index in self:
+            if index[0] == word:
+                return index[1]
+        else:
+            return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
+        for index in range(len(self)):
+            if self[index][0] == word:
+                return True
+        else:
+            return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
-        # TODO: Implement linear search to find index of entry with target word
+        for index in range(len(self)):
+            if self[index][0] == target:
+                return index
+            return None
 
 
 def print_histogram(word_list):
