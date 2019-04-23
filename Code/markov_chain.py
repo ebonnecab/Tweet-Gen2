7930bf3ad530658c5205e637a2fdb185e84b3918
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-22 19:24:25 -0700

    refactored markov chain

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -7,43 +7,62 @@ def get_corpus(file):
     corpus = get_words(file)
     return corpus
 
+def get_pairs(corpus):
+    for index in range(len(corpus)-1):
+        yield(corpus[index], corpus[index+1])
+
 def markov_walk(corpus):
     markov_dict = {}
-    
-    for word in corpus:
-        if word not in markov_dict:
-            markov_dict[word] = {}
-    
-    index = 0
-    if index + 1 < len(corpus):
-        next_word = corpus[index+1]
-        if next_word in markov_dict[word].keys():
-            markov_dict[word][next_word] += 1
+    pairs = get_pairs(corpus)
+
+    for first_word, second_word in pairs:
+        if first_word in markov_dict.keys():
+            markov_dict[first_word].append(second_word)
         else:
-            markov_dict[word][next_word] = 1
-        index +=1
-    print(index)
+            markov_dict[first_word] = [second_word]
+
     return markov_dict
 
-def start_word(markov_dict, word):
-    rand_word = random.choice(list(markov_dict.keys()))
-    return rand_word
+# def markov_walk(corpus):
+#     markov_dict = {}
+    
+#     for word in corpus:
+#         if word not in markov_dict:
+#             markov_dict[word] = {}
+    
+#     index = 0
+#     if index + 1 < len(corpus):
+#         next_word = corpus[index+1]
+#         if next_word in markov_dict[word].keys():
+#             markov_dict[word][next_word] += 1
+#         else:
+#             markov_dict[word][next_word] = 1
+#         index +=1
+#     print(index)
+#     return markov_dict
 
-def sentence_generator(markov_dict):
-    length = 10
-    first_word = list(markov_dict.keys())[0]
-    second_word = start_word(markov_dict, first_word)
-    sentence = first_word + ' ' + second_word
-    prev_word = second_word
+# def start_word(markov_dict, word):
+#     rand_word = random.choice(list(markov_dict.keys()))
+#     return rand_word
 
-    for word in range(0, random.randint(1, length)):
-        next_word = start_word(markov_dict, prev_word)
-        prev_word = next_word
-        sentence += next_word + ' '
-    return sentence
+# def generate_sentence(markov_dict):
+#     length = 10
+#     first_word = list(markov_dict.keys())[0]
+#     second_word = start_word(markov_dict, first_word)
+#     sentence = first_word + ' ' + second_word
+#     prev_word = second_word
+
+#     for word in range(0, random.randint(1, length)):
+#         next_word = start_word(markov_dict, prev_word)
+#         prev_word = next_word
+#         sentence += next_word + ' '
+#     return sentence
 if __name__ == '__main__':
-    corpus = get_corpus('siddhartha.txt')
+    corpus = get_corpus('fish.txt')
+    # pairs = get_pairs(corpus)
+    # print(pairs)
     chain = markov_walk(corpus)
-    sentence = sentence_generator(chain)
-    print(sentence)
+    print(chain)
+    # sentence = generate_sentence(chain)
+    # print(sentence)
     
