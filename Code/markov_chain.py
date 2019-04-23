f1ba848e4897c2fbd63a755f9f36faf09146ca82
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-23 09:42:21 -0700

    refactored markov chain

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -3,15 +3,25 @@ from histogram import get_words
 import random
 from dictogram import Dictogram
 
-
+'''
+uses imported function to get individual words from corpus
+'''
 def get_corpus(file):
     corpus = get_words(file)
     return corpus
 
+'''
+this function accesses each word in the corpus
+yields that word + the word that follows
+'''
 def get_pairs(corpus):
     for index in range(len(corpus)-1):
         yield(corpus[index], corpus[index+1])
 
+'''
+this function stores each individual word as keys
+and appends their pairs as values for that key
+'''
 def markov_walk(corpus):
     markov_dict = {}
     pairs = get_pairs(corpus)
@@ -24,26 +34,23 @@ def markov_walk(corpus):
 
     return markov_dict
 
-def start_word(markov_dict, word):
+def start_word(markov_dict):
     rand_word = random.choice(list(markov_dict.keys()))
     return rand_word
 
 def generate_sentence(markov_dict):
     length = 10
     first_word = list(markov_dict.keys())[0]
-    second_word = start_word(markov_dict, first_word)
+    second_word = start_word(markov_dict)
     sentence = first_word + ' ' + second_word
-    prev_word = second_word
 
     for word in range(0, random.randint(1, length)):
-        next_word = start_word(markov_dict, prev_word)
-        prev_word = next_word
+        next_word = start_word(markov_dict)
         sentence += ' ' + next_word + ' '
     return sentence
 if __name__ == '__main__':
-    corpus = get_corpus('fish.txt')
+    corpus = get_corpus('siddhartha.txt')
     chain = markov_walk(corpus)
-    print(chain)
     sentence = generate_sentence(chain)
     print(sentence)
     
