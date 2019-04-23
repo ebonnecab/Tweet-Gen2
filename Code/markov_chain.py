5094cc5eb7100bd74e420577a1694bbd669c37d5
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-22 20:29:23 -0700

    cleaned up code need to add comments

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -3,6 +3,7 @@ from histogram import get_words
 import random
 from dictogram import Dictogram
 
+
 def get_corpus(file):
     corpus = get_words(file)
     return corpus
@@ -23,30 +24,26 @@ def markov_walk(corpus):
 
     return markov_dict
 
+def start_word(markov_dict, word):
+    rand_word = random.choice(list(markov_dict.keys()))
+    return rand_word
 
+def generate_sentence(markov_dict):
+    length = 10
+    first_word = list(markov_dict.keys())[0]
+    second_word = start_word(markov_dict, first_word)
+    sentence = first_word + ' ' + second_word
+    prev_word = second_word
 
-# def start_word(markov_dict, word):
-#     rand_word = random.choice(list(markov_dict.keys()))
-#     return rand_word
-
-# def generate_sentence(markov_dict):
-#     length = 10
-#     first_word = list(markov_dict.keys())[0]
-#     second_word = start_word(markov_dict, first_word)
-#     sentence = first_word + ' ' + second_word
-#     prev_word = second_word
-
-#     for word in range(0, random.randint(1, length)):
-#         next_word = start_word(markov_dict, prev_word)
-#         prev_word = next_word
-#         sentence += next_word + ' '
-#     return sentence
+    for word in range(0, random.randint(1, length)):
+        next_word = start_word(markov_dict, prev_word)
+        prev_word = next_word
+        sentence += next_word + ' '
+    return sentence
 if __name__ == '__main__':
     corpus = get_corpus('fish.txt')
-    # pairs = get_pairs(corpus)
-    # print(pairs)
     chain = markov_walk(corpus)
-    print(chain)
-    # sentence = generate_sentence(chain)
-    # print(sentence)
+    # print(chain)
+    sentence = generate_sentence(chain)
+    print(sentence)
     
